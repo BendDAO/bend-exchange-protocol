a6abbfc73a4e264ad7608ebe6e781afbcdae7591
Thor Seldon <thorseldon@protonmail.com>
2024-01-12 15:19:02 +0800

    chore: Deploy new strategy for BIP#49

--- tasks/deploy.ts
+++ tasks/deploy.ts
@@ -113,6 +113,13 @@ task("deploy:Strategy-Zero-Fee", "Deploy Strategy").setAction(async (_, { networ
   const strategyStandardSaleForFixedPrice = await deployContract("StrategyStandardSaleForFixedPrice", [protocolFee]);
 });
 
+task("deploy:Strategy-100-Fee", "Deploy Strategy").setAction(async (_, { network, run }) => {
+  await run("set-DRE");
+  await run("compile");
+  const protocolFee = 100;
+  const strategyStandardSaleForFixedPrice = await deployContract("StrategyStandardSaleForFixedPrice", [protocolFee]);
+});
+
 task("deploy:RedeemNFT", "Deploy RedeemNFT").setAction(async (_, { run }) => {
   await run("set-DRE");
   await run("compile");
